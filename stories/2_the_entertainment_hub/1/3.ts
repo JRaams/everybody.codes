049e60b37b221c08f4e72b76cdc3f2a6855e305e
JRaams <47593569+JRaams@users.noreply.github.com>
2025-08-30 14:51:36 +0200

    story 2 quest 1 massive speedup in part 3

--- stories/2_the_entertainment_hub/1/3.ts
+++ stories/2_the_entertainment_hub/1/3.ts
@@ -6,48 +6,55 @@ const lines = await Bun.file(__dirname + "/input.txt").text();
 const [machineRaw, tokensRaw] = lines.trim().split("\n\n");
 const machine = machineRaw!.split("\n").map((line) => line.split(""));
 
-const SLOTS = machine[0].filter(x => x === '*').length;
-const allResults: number[][] = [];
+const slotCount = machine[0].filter((x) => x === "*").length;
+const scores = new Map<number, Map<number, number>>();
 
-tokensRaw!.split("\n").forEach((token) => {
-  const results: number[] = [];
+tokensRaw!.split("\n").forEach((token, i) => {
+  scores.set(i, new Map<number, number>());
 
-  for (let slot = 0; slot < SLOTS; slot++) {
+  for (let slot = 0; slot < slotCount; slot++) {
     const finalSlot = findFinalSlot(machine, token, slot);
-    const coins = Math.max(0, (finalSlot * 2) - slot - 1); 
-    results.push(coins);
+    const coins = Math.max(0, finalSlot * 2 - slot - 1);
+    scores.get(i)!.set(slot, coins);
   }
-
-  allResults.push(results);
 });
 
-function* permute(perm: number[], pos: number, options: number[], used: Set<number>): Generator<number[]> {
-  if (pos === perm.length) {
-    yield [...perm];
-  } else {
-    for (let i = 0; i < options.length; i++) {
-      if (!used.has(options[i]!)) {
-        used.add(options[i]!);
-        perm[pos] = options[i]!;
-        yield* permute(perm, pos + 1, options, used);
-        used.delete(options[i]!);
-      }
+function findLimit(
+  scores: Map<number, Map<number, number>>,
+  token: number,
+  usedSlots: number,
+  getMin: boolean,
+  cache: Map<string, number>
+): number {
+  if (!scores.has(token)) return 0;
+
+  const key = `${token},${usedSlots}`;
+  if (cache.has(key)) return cache.get(key)!;
+
+  let result = getMin ? Number.MAX_SAFE_INTEGER : 0;
+
+  for (const slot of scores.get(token)!.keys()) {
+    if (usedSlots & (1 << slot)) continue;
+
+    const newUsed = usedSlots | (1 << slot);
+
+    const score =
+      findLimit(scores, token + 1, newUsed, getMin, cache) +
+      scores.get(token)!.get(slot)!;
+
+    if (getMin) {
+      result = Math.min(result, score);
+    } else {
+      result = Math.max(result, score);
     }
   }
+
+  cache.set(key, result);
+  return result;
 }
 
+const max = findLimit(scores, 0, 0, false, new Map<string, number>());
 
-let min = Number.MAX_SAFE_INTEGER;
-let max = 0;
-
-for (const p of permute(Array(6).fill(-1), 0, Array(SLOTS).fill(0).map((_, i) => i), new Set())) {
-  let total = 0;
-  for (let i = 0; i < 6; i++) {
-    total += allResults[i][p[i]];
-  }
-
-  min = Math.min(min, total);
-  max = Math.max(max, total);
-}
+const min = findLimit(scores, 0, 0, true, new Map<string, number>());
 
 console.log(min, max);
