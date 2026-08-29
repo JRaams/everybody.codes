3412578d2c29d3faba6c75d54e8348585e089af8
JRaams <47593569+JRaams@users.noreply.github.com>
2026-08-29 12:49:43 +0200

    cleanup story 4 quest 3

--- stories/4_the_digital_ateltier/3/3.ts
+++ stories/4_the_digital_ateltier/3/3.ts
@@ -3,112 +3,64 @@ import { Grid, parse } from "./grid";
 const { width, height, horizontalOffsets, verticalOffsets } =
   await parse("3.txt");
 
-const grid = new Grid(verticalOffsets.length * 2, horizontalOffsets.length * 2);
+const patternWidth = verticalOffsets.length * 2;
+const patternHeight = horizontalOffsets.length * 2;
+
+const grid = new Grid(patternWidth, patternHeight);
 
 grid.markEdges(horizontalOffsets, verticalOffsets);
 
 grid.markGroups();
 
-grid.print();
+const frequencyX = Math.floor(width / patternWidth);
+const frequencyY = Math.floor(height / patternHeight);
 
-let green = 0;
-let yellow = 0;
+const remainderX = width % patternWidth;
+const remainderY = height % patternHeight;
 
-for (let y = 0; y < grid.cells.length; y++) {
-  for (let x = 0; x < grid.cells[0].length; x++) {
-    const cell = grid.cells[y][x];
-    if (!cell.isIsolated) continue;
+function countGroups(height: number, width: number, frequency: number) {
+  let green = 0;
+  let yellow = 0;
 
-    if (cell.group === "green") green++;
-    else if (cell.group === "yellow") yellow++;
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      const cell = grid.cells[y][x];
+      if (!cell.isIsolated) continue;
+
+      if (cell.group === "green") green++;
+      else if (cell.group === "yellow") yellow++;
+    }
   }
+
+  green *= frequency;
+  yellow *= frequency;
+
+  return [green, yellow];
 }
 
-const repeatsHorizontal = Math.floor(width / (verticalOffsets.length * 2));
-const repeatsVertical = Math.floor(height / (horizontalOffsets.length * 2));
+const [mainGreen, mainYellow] = countGroups(
+  patternHeight,
+  patternWidth,
+  frequencyX * frequencyY,
+);
 
-const mainGreen = green * repeatsHorizontal * repeatsVertical;
-const mainYellow = yellow * repeatsHorizontal * repeatsVertical;
+const [rightGreen, rightYellow] = countGroups(
+  patternHeight,
+  remainderX,
+  frequencyY,
+);
 
-const remainderHorizontal = width % (verticalOffsets.length * 2);
-const remainderVertical = height % (horizontalOffsets.length * 2);
+const [diagonalGreen, diagonalYellow] = countGroups(remainderY, remainderX, 1);
 
-console.log({
-  green,
-  yellow,
-  mainGreen,
-  mainYellow,
-  width,
-  height,
-  repeatsHorizontal,
-  repeatsVertical,
-  remainderHorizontal,
-  remainderVertical,
-});
-
-// right
-
-let rightGreen = 0;
-let rightYellow = 0;
-
-for (let y = 0; y < horizontalOffsets.length * 2; y++) {
-  for (let x = 0; x < remainderHorizontal; x++) {
-    const cell = grid.cells[y]?.[x];
-    if (!cell?.isIsolated) continue;
-
-    if (cell.group === "green") rightGreen++;
-    else if (cell.group === "yellow") rightYellow++;
-  }
-}
-
-rightGreen *= repeatsVertical;
-rightYellow *= repeatsVertical;
-
-// diagonal
-
-let diagonalGreen = 0;
-let diagonalYellow = 0;
-
-for (let y = 0; y < remainderVertical; y++) {
-  for (let x = 0; x < remainderHorizontal; x++) {
-    const cell = grid.cells[y]?.[x];
-    if (!cell?.isIsolated) continue;
-
-    if (cell.group === "green") diagonalGreen++;
-    else if (cell.group === "yellow") diagonalYellow++;
-  }
-}
-
-// bottom
-
-let bottomGreen = 0;
-let bottomYellow = 0;
-
-for (let y = 0; y < remainderVertical; y++) {
-  for (let x = 0; x < verticalOffsets.length * 2; x++) {
-    const cell = grid.cells[y]?.[x];
-    if (!cell?.isIsolated) continue;
-
-    if (cell.group === "green") bottomGreen++;
-    else if (cell.group === "yellow") bottomYellow++;
-  }
-}
-
-bottomGreen *= repeatsHorizontal;
-bottomYellow *= repeatsHorizontal;
+const [bottomGreen, bottomYellow] = countGroups(
+  remainderY,
+  patternWidth,
+  frequencyX,
+);
 
 const totalGreen = mainGreen + rightGreen + diagonalGreen + bottomGreen;
 const totalYellow = mainYellow + rightYellow + diagonalYellow + bottomYellow;
 
-console.log({
-  rightGreen,
-  rightYellow,
-  diagonalGreen,
-  diagonalYellow,
-  bottomGreen,
-  bottomYellow,
-  totalGreen,
-  totalYellow,
-});
+const largestGroup = Math.max(totalGreen, totalYellow);
 
-console.log(Math.max(totalGreen, totalYellow));
+console.log(largestGroup);
